9587125606a48f9b0ee1be36acda3680e3ef56de
damia <damia@DESKTOP-G1NUIKK>
2021-12-16 22:32:35 +0100

    Add project files.

---
+++ L2_7_Zad_11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace L2_7_Zad_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program zmieni ocenę ucznia na jej opis.");
+            Console.Write("Podaj ocenę od 1 do 6: ");
+            Int32.TryParse(Console.ReadLine(), out int rating);
+
+            switch (rating)
+            {
+                case 1:
+                    Console.WriteLine("Niedostateczny");
+                    break;
+                case 2:
+                    Console.WriteLine("Dopuszczający");
+                    break;
+                case 3:
+                    Console.WriteLine("Dostateczny");
+                    break;
+                case 4:
+                    Console.WriteLine("Dobry");
+                    break;
+                case 5:
+                    Console.WriteLine("Bardzo dobry");
+                    break;
+                case 6:
+                    Console.WriteLine("Celujący");
+                    break;
+                default:
+                    Console.WriteLine("Podana liczba nie zawiera się w systemie ocen.");
+                    break;
+            }
+        }
+    }
+}
